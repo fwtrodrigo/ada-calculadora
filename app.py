9fa745fbc1b31a50767e8725564342892b03c97d
Rodrigo Freitas <fwtrodrigo@gmail.com>
2024-09-05 04:19:47 -0300

    feat: add division operation

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ def calculator():
         "+": add,
         "-": subtract,
         "*": multiply,
+        "/": divide,
     }
 
     while True:
@@ -17,17 +18,20 @@ def calculator():
         num1 = get_number("Informe o primeiro numero: ")
         num2 = get_number("Informe o segundo numero: ")
 
-        result = operations[operation](num1, num2)
-        print(f"Result: {result}")
+        try:
+            result = operations[operation](num1, num2)
+            print(f"Result: {result}")
+        except ValueError as e:
+            print(f"Erro: {e}")
 
 
 def get_operation():
     while True:
         operation = input(
-            "Informe o simbolo referente a operacao desejada ('+', '-', '*',) ou '9' para sair do programa: "
+            "Informe o simbolo referente a operacao desejada ('+', '-', '*', '/') ou '9' para sair do programa: "
         )
 
-        if operation not in ("+", "-", "*", "9"):
+        if operation not in ("+", "-", "*", "/", "9"):
             print("Operacao invalida. Favor, informe uma das opcoes apresentadas")
             continue
 
@@ -52,6 +56,12 @@ def subtract(num1, num2):
 
 def multiply(x, y):
     return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Nao é possivel dividir por zero.")
+    return x / y
 
 
 if __name__ == "__main__":
