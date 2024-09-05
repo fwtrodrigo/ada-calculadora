e48ffddbb59f6b41953a85691619638a10fa2696
Rodrigo Freitas <fwtrodrigo@gmail.com>
2024-09-05 04:06:26 -0300

    feat: add addition operation

---
+++ app.py
@@ -0,0 +1,48 @@
+def calculator():
+    print("Bem vindo a calculadora!")
+
+    operations = {
+        "+": add,
+    }
+
+    while True:
+        operation = get_operation()
+
+        if operation == "9":
+            print("Programa finalizado")
+            exit(0)
+
+        num1 = get_number("Informe o primeiro numero: ")
+        num2 = get_number("Informe o segundo numero: ")
+
+        result = operations[operation](num1, num2)
+        print(f"Result: {result}")
+
+
+def get_operation():
+    while True:
+        operation = input(
+            "Informe o simbolo referente a operacao desejada ('+') ou '9' para sair do programa: "
+        )
+
+        if operation not in ("+","9"):
+            print("Operacao invalida. Favor, informe uma das opcoes apresentadas")
+            continue
+
+        return operation
+
+
+def get_number(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Favor, digite um numero valido.")
+
+
+def add(x, y):
+    return x + y
+
+
+if __name__ == "__main__":
+    calculator()
